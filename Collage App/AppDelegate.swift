6a3f8f06746566b149fa0fb00e2f242038c45547
Luis Arias <lugearma@gmail.com>
2016-09-28 01:19:13 -0500

    added navigation controller

--- Collage App/AppDelegate.swift
+++ Collage App/AppDelegate.swift
@@ -13,9 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+    
+        let flowLayout = UICollectionViewFlowLayout()
+        let collageView = CollageViewController(collectionViewLayout: flowLayout)
+        let navigationController = UINavigationController(rootViewController: collageView)
+        
+        window?.rootViewController = navigationController
+        
         return true
     }
 
---
+++ Collage App/CollageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Collage App
+//
+//  Created by Luis Arias on 27/09/16.
+//  Copyright © 2016 Luis Arias. All rights reserved.
+//
+
+import UIKit
+
+class CollageViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.lightGray
+    }
+}
+
